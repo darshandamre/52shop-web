0c667f8a5197cf1adece3c460bcaf6b082c73935
Darshan Damre <darshandamre5@gmail.com>
2022-03-25 17:10:02 +0530

    updated useForm hook with useReducer

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,79 +1,91 @@
-import { useState } from "react";
+import { useCallback, useReducer } from "react";
 
 const createTouched = (object, bool) => {
   let result = {};
-
   for (const key in object) {
     result = {
       ...result,
       [key]: bool
     };
   }
-
   return result;
+};
+
+const formReducer = (state, { type, payload, validatorFn }) => {
+  switch (type) {
+    case "ON_CHANGE":
+      const newValues = {
+        ...state.values,
+        [payload.name]: payload.value
+      };
+      return {
+        ...state,
+        values: newValues,
+        errors: validatorFn(newValues)
+      };
+
+    case "ON_BLUR":
+      const { touched } = state;
+      if (touched[payload.name]) {
+        return state;
+      }
+      return {
+        ...state,
+        touched: {
+          ...touched,
+          [payload.name]: true
+        }
+      };
+
+    case "SET_SUBMITTING":
+      return {
+        ...state,
+        isSubmitting: payload,
+        touched: payload ? createTouched(state.values, true) : state.touched
+      };
+
+    case "SET_ERRORS":
+      return {
+        ...state,
+        errors: payload
+      };
+
+    default:
+      throw new Error(`invalid action ${type}`);
+  }
 };
 
 // initialInputState is an object of the state of input components
 // validaor fn is a function that checks for the input values
 // and returns the error in them
 export const useForm = (initialInputState, validatorFn) => {
-  const [formState, setFormState] = useState({
+  const [formState, dispatch] = useReducer(formReducer, {
     values: initialInputState,
     errors: validatorFn(initialInputState),
     touched: createTouched(initialInputState, false),
     isSubmitting: false
   });
 
-  const onChange = e =>
-    setFormState(({ values: prevValues, ...prevState }) => {
-      const newValues = {
-        ...prevValues,
-        [e.target.name]: e.target.value
-      };
+  const onChange = useCallback(
+    e => dispatch({ type: "ON_CHANGE", payload: e.target, validatorFn }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
 
-      return {
-        ...prevState,
-        values: newValues,
-        errors: validatorFn(newValues)
-      };
-    });
+  const onBlur = useCallback(
+    e => dispatch({ type: "ON_BLUR", payload: e.target }),
+    []
+  );
 
-  const onBlur = e =>
-    setFormState(prevState => {
-      const { touched: prevTouched } = prevState;
+  const setSubmitting = useCallback(
+    trueOrFalse => dispatch({ type: "SET_SUBMITTING", payload: trueOrFalse }),
+    []
+  );
 
-      if (prevTouched[e.target.name]) {
-        return prevState;
-      }
-
-      return {
-        ...prevState,
-        touched: {
-          ...prevTouched,
-          [e.target.name]: true
-        }
-      };
-    });
-
-  const setSubmitting = functionOrBoolean =>
-    setFormState(({ isSubmitting, touched, ...prevState }) => {
-      const result =
-        typeof functionOrBoolean === "function"
-          ? functionOrBoolean(isSubmitting)
-          : functionOrBoolean;
-
-      return {
-        ...prevState,
-        isSubmitting: result,
-        touched: result ? createTouched(initialInputState, true) : touched
-      };
-    });
-
-  const setErrors = errors =>
-    setFormState(prevState => ({
-      ...prevState,
-      errors
-    }));
+  const setErrors = useCallback(
+    errors => dispatch({ type: "SET_ERRORS", payload: errors }),
+    []
+  );
 
   const { values, errors, touched, isSubmitting } = formState;
 
