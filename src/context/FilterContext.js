7df4fc14d8399ab72162672ae80aaf83878a9a60
Darshan Damre <darshandamre5@gmail.com>
2022-04-02 12:40:35 +0530

    added filters context

---
+++ src/context/FilterContext.js
@@ -0,0 +1,34 @@
+import * as JSURL from "jsurl";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
+import { filtersReducer, initialfilters } from "../reducer/filterReducer";
+
+const key = "filters";
+
+const FiltersContext = createContext();
+
+const FiltersProvider = ({ children }) => {
+  const { state } = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const paramValue = JSURL.parse(searchParams.get(key));
+  const [filters, dispatchFilters] = useReducer(
+    filtersReducer,
+    state ? { ...initialfilters, ...state } : paramValue ?? initialfilters
+  );
+
+  useEffect(() => {
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set(key, JSURL.stringify(filters));
+    setSearchParams(newSearchParams, { replace: true });
+  }, [filters, searchParams, setSearchParams]);
+
+  return (
+    <FiltersContext.Provider value={{ filters, dispatchFilters }}>
+      {children}
+    </FiltersContext.Provider>
+  );
+};
+
+const useFilters = () => useContext(FiltersContext);
+
+export { FiltersProvider, useFilters };
