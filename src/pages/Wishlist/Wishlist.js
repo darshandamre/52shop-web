84b9b074aa6fc10447488a0651bf08442ee7ece8
Darshan Damre <darshandamre5@gmail.com>
2022-04-05 17:29:16 +0530

    added link to products page in empty wishlist

--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card } from "../../components";
 import { useUserQuery } from "../../hooks";
 import "./Wishlist.css";
@@ -9,6 +10,17 @@ const Wishlist = () => {
   return (
     <main className="wishlist mb-8">
       <h3 className="h3 ta-center mt-3 mb-6">Wishlist</h3>
+
+      {data?.user.wishlist.length === 0 ? (
+        <div className="ta-center mx-2">
+          No items in your wishlist{" "}
+          <Link to="/products" className="link">
+            go to products
+          </Link>{" "}
+          page and add products in your wishlist.
+        </div>
+      ) : null}
+
       <div className="container">
         {data?.user.wishlist.map(product => (
           <Card page="wishlist" key={product.id} product={product} />
