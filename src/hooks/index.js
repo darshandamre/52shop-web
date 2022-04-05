73e3ea380007d619ed0cf6b958190b2892a8ce1a
Darshan Damre <darshandamre5@gmail.com>
2022-04-05 17:12:45 +0530

    added add-to-wishlist and remove-from-wishlist mutations

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,3 +3,5 @@ export { useLoginMutation } from "./useLoginMutation";
 export { useProductsQuery } from "./useProductsQuery";
 export { useSignupMutation } from "./useSignupMutation";
 export { useUserQuery } from "./useUserQuery";
+export { useAddToWishlistMutation } from "./useAddToWishlistMutation";
+export { useRemoveFromWishlistMutation } from "./useRemoveFromWishlistMutation";
---
+++ src/hooks/useRemoveFromWishlistMutation.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useMutation } from "react-query";
+import { queryClient } from "../queryClient";
+import { API } from "../utils/constants";
+import { getToken } from "../utils/token";
+
+export const useRemoveFromWishlistMutation = () =>
+  useMutation(
+    async productId =>
+      await axios.delete(`${API}/user/wishlist/${productId}`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`
+        }
+      }),
+    {
+      onError: err => console.error(err),
+      onSuccess: (_, productId) =>
+        queryClient.setQueryData("user", ({ user }) => ({
+          user: {
+            ...user,
+            wishlist: user.wishlist.filter(item => item.id !== productId)
+          }
+        }))
+    }
+  );
