7d120ac195a968c29fe1928080ebde803230b4da
Darshan Damre <darshandamre5@gmail.com>
2022-04-05 17:13:56 +0530

    updated card for wishlist management

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  useAddToWishlistMutation,
+  useRemoveFromWishlistMutation,
+  useUserQuery
+} from "../../hooks";
 import "./Card.css";
 
 const Card = ({ product }) => {
-  const { name, price, discount, pictureLink, cartItem } = product;
+  const { mutate: addToWishlist } = useAddToWishlistMutation();
+  const { mutate: removeFromWishlist } = useRemoveFromWishlistMutation();
+  const { data } = useUserQuery();
+  const { user } = data ?? {};
+  const navigate = useNavigate();
+
+  const { id, name, price, discount, pictureLink, cartItem } = product;
+
+  const wishlisted = user?.wishlist?.some(item => item.id === id);
+
+  const wishlistHandler = () => {
+    if (!user?.id) {
+      return navigate("/signup");
+    }
+    return wishlisted ? removeFromWishlist(id) : addToWishlist(id);
+  };
 
   return (
     <div className="card">
-      <div className="card__fav">
-        <i className="fa-regular fa-heart"></i>
+      <div className="card__fav" onClick={wishlistHandler}>
+        <i className={`${wishlisted ? "fa-solid" : "fa-regular"} fa-heart`}></i>
       </div>
       <img className="card__image" src={pictureLink} alt={name} />
       <div className="card__content">
