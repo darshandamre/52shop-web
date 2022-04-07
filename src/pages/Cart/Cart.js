88543e509c6eb38f37779c62375db00e1df67bc3
Darshan Damre <darshandamre5@gmail.com>
2022-04-07 16:25:33 +0530

    added cart page

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Card } from "../../components";
+import { useUserQuery } from "../../hooks";
+import "./Cart.css";
+
+const Cart = () => {
+  const { data } = useUserQuery();
+  const { user } = data ?? {};
+
+  const totalItems = user?.cart?.reduce(
+    (sum, { cartItem }) => sum + cartItem.quantity,
+    0
+  );
+  const totalPrice = user?.cart?.reduce(
+    (sum, { price, cartItem }) => sum + price * cartItem.quantity,
+    0
+  );
+  const totalDiscount = user?.cart?.reduce(
+    (sum, { discount, cartItem }) => sum + discount * cartItem.quantity,
+    0
+  );
+  const deliveryCharges = totalPrice > 1000 ? 100 : 300;
+  const finalAmount = totalPrice + deliveryCharges - totalDiscount;
+
+  return (
+    <main className="cart">
+      <div className="container">
+        <h3 className="h3 ta-center mb-6">Cart ({totalItems})</h3>
+
+        <div className="cart__content">
+          <div className="cart__details p-6">
+            <p className="cart__details__title fw-600">PRICE DETAILS</p>
+            <ul className="price__list">
+              <li className="price__item">
+                Price ({totalItems} items) <span>&#8377; {totalPrice}</span>
+              </li>
+              <li className="price__item">
+                Discount <span>- &#8377; {totalDiscount} </span>
+              </li>
+              <li className="price__item">
+                Delivery Charges <span>&#8377; {deliveryCharges} </span>
+              </li>
+              <li className="price__item fw-600">
+                TOTAL AMOUNT <span>&#8377; {finalAmount}</span>
+              </li>
+            </ul>
+            <p className="pb-3">
+              You will save &#8377; {totalDiscount} on this order
+            </p>
+            <button className="btn btn--primary width-100 m-0">
+              PLACE ORDER
+            </button>
+          </div>
+
+          {user?.cart?.map(product => {
+            return <Card key={product.id} product={product} />;
+          })}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export { Cart };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export { Login } from "./Auth/Login";
 export { SignUp } from "./Auth/SignUp";
+export { Cart } from "./Cart/Cart";
 export { Home } from "./Home/Home";
 export { Product } from "./Product/Product";
 export { Wishlist } from "./Wishlist/Wishlist";
